7cfc0a57b3ec4bb68e98f092c7e987de9bd5d2e0
p_craft <ef81sp@gmail.com>
2024-11-02 15:35:31 +0900

    fix

--- cli.js
+++ cli.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const fs = require('fs');
 
-execSync('textlint "./docs/**/*.html"', { stdio: 'inherit' });
+const args = process.argv.slice(2);
+const directory = args[0];
+if (!directory) {
+  console.error('Error: No directory specified.');
+  process.exit(1);
+}
+if (!fs.existsSync(directory)) {
+  console.error('Error: Directory does not exist.');
+  process.exit(1);
+}
+execSync(`textlint "${directory}"`, { stdio: 'inherit' });
