57b9b56fca435c4aa822ac429c47c786643f29e3
p_craft <ef81sp@gmail.com>
2024-11-02 15:39:58 +0900

    fix

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const args = process.argv.slice(2);
-const directory = args[0];
+const directory = args[1];
 if (!directory) {
   console.error('Error: No directory specified.');
   process.exit(1);
